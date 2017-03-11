9ccd58cefa43c92ce6f1124be8ef25d0a0608043
Andrew McDermott <aim@frobware.com>
2017-03-11 22:54:02 +0000

    working on coverage

--- lcov.bash
+++ lcov.bash
@@ -4,7 +4,9 @@ lcov --directory . --zerocounters
 make clean
 make
 ./bin/test-message
+./bin/test-mprintf
+./bin/test-allocator
 lcov --directory . --capture --output-file coverage.info
-lcov --remove coverage.info 'lib/test/*' '/usr/*' --output-file coverage.final
+lcov --remove coverage.info '/usr/*' --output-file coverage.final
 mv coverage.final coverage.info
 genhtml -o coveragereport coverage.info
