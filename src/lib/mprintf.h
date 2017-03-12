3c818a2c94de52b3435cc84ab004b5518b8f11f1
Andrew McDermott <aim@frobware.com>
2017-03-12 18:45:11 +0000

    add format(printf, 2, 3) checks to mprintf

--- src/lib/mprintf.h
+++ src/lib/mprintf.h
@@ -2,7 +2,4 @@
 
 #include <dsio/allocator.h>
 
-char *dsio_mprintf(const struct dsio_allocator *a, const char *format, ...) __attribute__ ((format(printf, 2, 3)));
-
-extern char *dsio_mprintf(const struct dsio_allocator *allocator,
-			  const char *fmt, ...);
+extern char *dsio_mprintf(const struct dsio_allocator *a, const char *format, ...) __attribute__ ((format(printf, 2, 3)));
