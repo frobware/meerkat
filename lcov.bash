43e7ffef157098c99e8447780702d0f735a764e0
Andrew McDermott <aim@frobware.com>
2017-03-22 20:40:24 +0000

    update lcov.sh with new build directory

--- lcov.bash
+++ lcov.bash
@@ -5,9 +5,9 @@ set -e
 lcov --directory . --zerocounters
 make clean
 make
-./bin/test-message
-./bin/test-mprintf
-./bin/test-allocator
+./build/bin/test-message
+./build/bin/test-mprintf
+./build/bin/test-allocator
 lcov --directory . --capture --output-file coverage.info
 lcov --remove coverage.info '/usr/*' --output-file coverage.final
 mv coverage.final coverage.info
