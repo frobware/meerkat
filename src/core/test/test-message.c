005316defffe290612469e5bf384f2585f130cfb
Andrew McDermott <aim@frobware.com>
2017-03-24 12:21:45 +0000

    correct name typo

--- src/core/test/test-message.c
+++ src/core/test/test-message.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2017 Andrew Mcdermott
+/* Copyright (C) 2017 Andrew McDermott
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
