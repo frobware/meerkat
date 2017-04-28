33a52d1a564a9e8e2d404442ad1d13a0d8264595
Andrew McDermott <aim@frobware.com>
2017-04-28 11:41:06 +0100

    add missing file message2.c

---
+++ src/core/message2.c
@@ -0,0 +1,151 @@
+/* Copyright (C) 2017 Andrew McDermott
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include <dsio/dsio.h>
+#include "message.h"
+#include "topics.h"
+#include "actions.h"
+#include "mprintf.h"
+
+struct scanner {
+	const char *input;
+	const char *curr;
+	struct dsio_msg *msg;
+	const struct dsio_allocator *allocator;
+	int parse_complete;
+};
+
+static int parse_topic(struct scanner *s)
+{
+	const char *token = s->curr;
+
+	for (; *s->curr != '\0'; s->curr++) {
+		switch (*s->curr) {
+		case DSIO_MSG_PART_SEPARATOR:
+			s->msg->topic = dsio_topic_lookup(token, s->curr - token);
+			s->curr++;
+			return s->msg->topic ? DSIO_OK : DSIO_ERROR;
+		}
+	}
+
+	return DSIO_ERROR;
+}
+
+static int parse_action(struct scanner *s)
+{
+	const char *token = s->curr;
+
+	for (; *s->curr != '\0'; s->curr++) {
+		int c = *s->curr;
+		switch (c) {
+		case DSIO_MSG_SEPARATOR:
+			s->parse_complete = 1;
+			/* fallthrough */
+		case DSIO_MSG_PART_SEPARATOR:
+			s->msg->action = dsio_action_lookup(token, s->curr - token);
+			s->curr++;
+			return s->msg->action ? DSIO_OK : DSIO_ERROR;
+		}
+	}
+
+	return DSIO_ERROR;
+}
+
+static int parse_payload(struct scanner *s)
+{
+	const char *token = s->curr;
+
+	for (; *s->curr != '\0'; s->curr++) {
+		switch (*s->curr) {
+		case DSIO_MSG_SEPARATOR:
+			s->parse_complete = 1;
+			return DSIO_OK;
+		case DSIO_MSG_PART_SEPARATOR:
+			s->msg->data = DSIO_REALLOC(s->allocator,
+						    s->msg->data,
+						    (1 + s->msg->ndata) * sizeof(*s->msg->data));
+			if (s->msg->data == NULL) {
+				return DSIO_NOMEM;
+			}
+			s->msg->data[s->msg->ndata].start = token;
+			s->msg->data[s->msg->ndata++].len = s->curr - token;
+			token = s->curr + 1;
+			break;
+		}
+	}
+
+	return DSIO_OK;
+}
+
+int dsio_msg_parse(const struct dsio_allocator *a, const char *input, struct dsio_msg *msg)
+{
+	int rc;
+	struct scanner s;
+
+	if (input == NULL || *input == '\0')
+		return DSIO_ERROR;
+
+	memset(msg, 0, sizeof *msg);
+	s.input = input;
+	s.curr = input;
+	s.parse_complete = 0;
+	s.msg = msg;
+	s.msg->raw = input;
+	s.allocator = a;
+
+	if ((rc = parse_topic(&s)) != DSIO_OK)
+		return rc;
+
+	if ((rc = parse_action(&s)) != DSIO_OK)
+		return rc;
+
+	if (!s.parse_complete) {
+		if ((rc = parse_payload(&s)) != DSIO_OK)
+			return rc;
+	}
+
+	return s.parse_complete ? DSIO_OK : DSIO_ERROR;
+}
+
+char *dsio_msg_create(const struct dsio_allocator *allocator,
+		      enum dsio_topic_tag topic,
+		      enum dsio_action_tag action,
+		      const char *payload)
+{
+	if (payload == NULL || *payload == '\0') {
+		return dsio_mprintf(allocator,
+				    "%s%c%s%c",
+				    dsio_topics[topic].ident,
+				    DSIO_MSG_PART_SEPARATOR,
+				    dsio_actions[action].ident,
+				    DSIO_MSG_SEPARATOR);
+	}
+	
+	return dsio_mprintf(allocator,
+			    "%s%c%s%c%s%c",
+			    dsio_topics[topic].ident,
+			    DSIO_MSG_PART_SEPARATOR,
+			    dsio_actions[action].ident,
+			    DSIO_MSG_PART_SEPARATOR,
+			    payload,
+			    DSIO_MSG_SEPARATOR);
+}
+
